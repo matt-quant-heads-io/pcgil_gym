20ea99000b02f96c1f81dffd3cc6c0d9790fb2e7
Emily Yu <eyudeveloper@gmail.com>
2021-04-06 13:21:15 -0700

    establish structure for using mult change% & trial

--- inference.py
+++ inference.py
@@ -3,37 +3,55 @@ import gym_pcgrl
 
 # assume input is from generated training eps via matt
 
+
+
 ###### determine unsuccessful/successful
 # env = gym.make('sokoban-narrow-v0')
-env = gym.make('zelda-wide-v0') # use zelda-wide-v0
+agents = ['zelda-wide-v0', 'zelda-narrow-v0', 'zelda-turtle-v0']
+env = gym.make(agents[0]) # use zelda-wide-v0
 obs = env.reset()
+trackingstruct = {}
+'''
+{
+  agent_name: [{
+    change_percentage: 0.1,
+    success_ct: 4,
+    failure_ct: 5,
+    total_runs: 9
+  }]
+}
+'''
+
 # [todo] clarify below values to use in change percenage (in inference.py)
-env.kwargs = { # set new kwargs
-    'change_percentage': 0.4,# ----- to use values of (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
-    'trials': 5,# ------------------ =40
-    'verbose': True
-    }
-#########################################
+vals = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+for val in vals:
+  env.kwargs = { # set new kwargs
+      'change_percentage': val, # to use values of (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+      'trials': 40, # 40 trials
+      'verbose': True
+      }
+  #########################################
 
-###### determine unsuccessful/successful
-# solver code for 1 trial
-for t in range(1000):
+  ###### determine unsuccessful/successful
+  # solver code for 1 trial
+  success = False
+  for t in range(1000):
 
-  # pick action here, create an [change, x, y] action using new agent
-  action = env.action_space.sample()
+    # pick action here, create an [change, x, y] action using new agent
+    action = env.action_space.sample()
 
-  obs, reward, done, info = env.step(action)
-  env.render('human')
-  if done: # mark as success
-    # print(obs)
-    # print(reward)
-    # print(done)
-    print(info) # {'player': 3, 'key': 2, 'door': 2, 'enemies': 7, 'regions': 5, 'nearest-enemy': 0, 'path-length': 0, 'iterations': 17, 'changes': 15, 'max_iterations': 1155, 'max_changes': 15}
-    # % tile-change actions: info.changes
-    print("Episode finished after {} timesteps".format(t+1))
-    break
-# if reach here, unsuccessful
-#########################################
+    obs, reward, done, info = env.step(action)
+    env.render('human')
+    if done: # mark as success
+      print(info) # {'player': 3, 'key': 2, 'door': 2, 'enemies': 7, 'regions': 5, 'nearest-enemy': 0, 'path-length': 0, 'iterations': 17, 'changes': 15, 'max_iterations': 1155, 'max_changes': 15}
+      # % tile-change actions: info.changes
+      print("Episode finished after {} timesteps".format(t+1))
+      success = True
+      break
+  # if reach here, unsuccessful
+  if not success:
+    print('unsuccessful operation')
+  #########################################
 
 ##### stats generation
 # generate matplotlib graph for above (bullet 1) - to confirm, bullet1 merges into the last line of bullet2
